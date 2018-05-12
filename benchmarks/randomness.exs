678df81b9cd012a5d6aba7669b6005eb60fb109d
Tobias Pfeiffer <pragtob@gmail.com>
2018-05-12 14:02:17 +0200

    random algorithms benchmark, might be better with nanosecond magic from benchee master

---
+++ benchmarks/randomness.exs
@@ -0,0 +1,29 @@
+algorithms = [:exs64, :exsplus, :exsp, :exs1024, :exs1024s, :exrop]
+
+jobs = Enum.map(algorithms, fn algorithm ->
+    {
+      "#{algorithm} uniform",
+      {
+        fn n -> :rand.uniform(n) end,
+        before_scenario: fn n ->
+          :rand.seed(algorithm)
+          n
+        end
+      }
+    }
+  end
+)
+
+inputs = %{
+  "100" => 100,
+  "10_000" => 10_000
+}
+
+Benchee.run(
+  jobs,
+  console: [extended_statistics: true],
+  inputs: inputs,
+  time: 2,
+  warmup: 1,
+  pre_check: true
+)
