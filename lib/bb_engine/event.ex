1c3d8272d9ed29c332a6dfbc56d71884c50bfd94
Tobias Pfeiffer <pragtob@gmail.com>
2019-04-11 10:41:56 +0200

    Show them why we need BBEngine.Event

--- lib/bb_engine/event.ex
+++ lib/bb_engine/event.ex
@@ -1,6 +1,19 @@
 defmodule BBEngine.Event do
   alias BBEngine.Event
 
+  @moduledoc """
+  Gathers all the event types for easy typing.
+
+  New events that concern an individual/include an event of statistical importance
+  should at least include the following field:
+
+  actor_id - who did this?
+  team - which team was the person on?
+  duration - how long did it take?
+
+  Other fields are free to be event specific.
+  """
+
   @type t ::
           Event.Rebound.t()
           | Event.Shot.t()
