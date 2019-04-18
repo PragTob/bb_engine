7a95ab4e6934a3fb9736c6f55547467c50f5cb51
Tobias Pfeiffer <pragtob@gmail.com>
2019-04-18 13:21:38 +0200

    test forced for end of shot clock

--- test/bb_engine/action/forced_test.exs
+++ test/bb_engine/action/forced_test.exs
@@ -12,4 +12,14 @@ defmodule BBEngine.Action.ForcedTest do
       gs
     end)
   end
+
+  test "doesn't take more time than possible with the shot clock running out" do
+    game_state = TestHelper.build_game_state(%{clock_seconds: 50, shot_clock: 1})
+
+    Enum.reduce(1..10, game_state, fn _, gs ->
+      {gs, event} = play(gs)
+      assert event.duration <= 1
+      gs
+    end)
+  end
 end
