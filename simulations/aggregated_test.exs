f8dc4b10466822603dc5300ada9cb9f54b6dc636
Tobias Pfeiffer <pragtob@gmail.com>
2019-06-25 17:25:51 +0200

    Fix benchmarks etc. in the face of substitutions

--- simulations/aggregated_test.exs
+++ simulations/aggregated_test.exs
@@ -8,12 +8,14 @@ road_players = Enum.map(13..24, &Player.standard_player/1)
 
 home_squad = %Squad{
   players: home_players,
-  lineup: [1, 2, 3, 4, 5]
+  lineup: [1, 2, 3, 4, 5],
+  bench: Enum.to_list(6..12)
 }
 
 road_squad = %Squad{
   players: road_players,
-  lineup: [13, 14, 15, 16, 17]
+  lineup: [13, 14, 15, 16, 17],
+  bench: Enum.to_list(18..25)
 }
 
 box_score =
